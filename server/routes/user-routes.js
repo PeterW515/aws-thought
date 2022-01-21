e9e81410462ae442bf689b28669dd93fbc693233
Peter Weigman <phweigman@gmail.com>
2022-01-21 18:48:15 -0500

    added image support to get route

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -30,12 +30,13 @@ router.get('/users/:username', (req, res) => {
         ExpressionAttributeNames: {
             "#un": "username",
             "#ca": "createdAt",
-            "#th": "thought"
+            "#th": "thought",
+            "#img": "image"
         },
         ExpressionAttributeValues: {
             ":user": req.params.username
         },
-        ProjectionExpression: "#un, #th, #ca",
+        ProjectionExpression: "#un, #th, #ca, #img",
         ScanIndexForward: false
     };
     dynamodb.query(params, (err, data) => {
