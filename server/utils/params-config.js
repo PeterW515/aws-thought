5d61bf9fdae113cdc1fe6670c7fda3778ab0836b
Peter Weigman <phweigman@gmail.com>
2022-01-20 22:17:58 -0500

    adding comments

--- server/utils/params-config.js
+++ server/utils/params-config.js
@@ -3,7 +3,7 @@ const { v4: uuidv4 } = require('uuid');
 const params = fileName => {
     const myFile = fileName.originalname.split('.');
     const fileType = myFile[myFile.length - 1];
-
+    //must review how this file is used
     const imageParams = {
         Bucket: 'user-images-5f7293b7-fece-46b1-80af-82d35074b48a',
         Key: `${uuidv4()}.${fileType}`,
